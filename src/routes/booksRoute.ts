2fcc06b857eb6bca2b16703ba78d307c636dc9c0
Avinash Malla <malla.avi@gmail.com>
2022-07-18 14:44:43 +0300

    added middlewares to books
    -books/middlewares
    -changed customError status type to int

--- src/routes/booksRoute.ts
+++ src/routes/booksRoute.ts
@@ -1,15 +1,15 @@
 import { Router, Request, Response, NextFunction } from "express"
-import { getAllBooks, putBooks, getBooksAllCategories, getBooksByCategory, getBookByISBN, getBookByTitle, getBooksOnLoan } from "../controllers/booksController"
-import { checkCategory, upperTitle } from "../middlewares/booksMiddleware"
+import booksController from "../controllers/booksController"
+import booksMiddleware from "../middlewares/booksMiddleware"
 
 const booksRoute = Router()
 
-booksRoute.get('', getAllBooks)
-booksRoute.put('', putBooks)
-booksRoute.get('/categories', getBooksAllCategories)
-booksRoute.get('/categories/:category', checkCategory, getBooksByCategory)
-booksRoute.get('/findByISBN/:isbn', getBookByISBN)
-booksRoute.get('/findByTitle/:title', upperTitle, getBookByTitle)
-booksRoute.get('/onLoan', getBooksOnLoan)
+booksRoute.get('', booksController.getAllBooks)
+booksRoute.put('', booksController.putBooks)
+booksRoute.get('/categories', booksController.getBooksAllCategories)
+booksRoute.get('/categories/:category', booksMiddleware.checkCategory, booksController.getBooksByCategory)
+booksRoute.get('/findByISBN/:isbn', booksController.getBookByISBN)
+booksRoute.get('/findByTitle/:title', booksMiddleware.upperTitle, booksController.getBookByTitle)
+booksRoute.get('/onLoan', booksController.getBooksOnLoan)
  
 export default booksRoute
