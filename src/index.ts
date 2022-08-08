79cce0bfcff25ebc2ccd07c588cf8d4b02d73122
Avinash Malla <malla.avi@gmail.com>
2022-08-08 10:35:39 +0300

    edited index.ts

--- src/index.ts
+++ src/index.ts
@@ -10,11 +10,13 @@ const database = process.env.MONGODB_DATABASE
 
 let uri = `mongodb+srv://${username}:${password}@${cluster}/${database}?retryWrites=true&w=majority`;
 
-mongoose.connect(uri)
-.then(() => {
-    app.listen(app.get('port'), () => console.log(`app is up and running in port ${ app.get('port') }`))
-}).catch(e => {
-    console.log('mongodb connection error')
-    // console.log(e)
-    process.exit(1)
-})
+app.listen(app.get('port'), () => console.log(`app is up and running in port ${ app.get('port') }`))
+
+// mongoose.connect(uri)
+// .then(() => {
+//     app.listen(app.get('port'), () => console.log(`app is up and running in port ${ app.get('port') }`))
+// }).catch(e => {
+//     console.log('mongodb connection error')
+//     // console.log(e)
+//     process.exit(1)
+// })
